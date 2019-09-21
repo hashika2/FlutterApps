8ff0c82d2fb6536c8416387edc293d6c852036c6
hashika <m.g.hashikamaduranga@gmail.com>
2019-09-21 23:56:55 +0530

    add code for wrinting and reading file

--- sharedapp/lib/main.dart
+++ sharedapp/lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:path_provider/path_provider.dart';
 
 void main(){
   runApp(new MaterialApp(
@@ -17,7 +20,40 @@ void main(){
       @override
       Widget build(BuildContext context) {
         return Container(
-           child: child,
+           
         );
+      }
+
+      Future<String> get _localPath async{
+        final directory=await getApplicationDocumentsDirectory();
+         return directory.path;//home.directory
+      }
+      Future <File> get _localFile async{
+         final path=await _localPath;
+
+         return new File('$path/data.txt');//home/directory/data.txt
+      }
+
+      //write and read from our file
+
+      Future<File> writeData(String message) async
+      {
+        final file=await _localFile;
+
+        //write to file4
+        return file.writeAsString('$message');
+      }
+
+      Future<String> readData() async{
+        try{
+          final file=await _localFile;
+
+          //read
+          String data=await file.readAsString();
+          return data;
+        }
+        catch (e){
+          return e;
+        }
       }
     }
