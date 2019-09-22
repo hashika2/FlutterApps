e1ad5873eda9600e4ea2647c0a174d197445367f
hashika <m.g.hashikamaduranga@gmail.com>
2019-09-22 11:32:19 +0530

    finished read and write data

--- sharedapp/lib/main.dart
+++ sharedapp/lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async{
 
@@ -26,6 +27,22 @@ void main() async{
     
     class _HomeState extends State<Home> {
       var _enterDataField=new TextEditingController();
+      String _saveData="";
+
+      @override
+  void initState() {
+  
+    super.initState();
+    _loadStateData();
+  }
+  _loadStateData() async{
+
+    sharedPreferences preferences=await SharedPreference.getInstace();
+
+    setState(() {
+     _saveData=preferences.getString ("Data");
+    });
+  }
       @override
       Widget build(BuildContext context) {
         return Scaffold(
@@ -54,7 +71,21 @@ void main() async{
                    children: <Widget>[
                      new Text("write some"),
                      Padding(padding: const EdgeInsets.all(14.5),),
-                     new Text("save data here")
+                     new FutureBuilder(
+                       future: readData(),
+                       builder: (BuildContext context,AsyncSnapshot<String> data){
+                         if(data.hasData !=null){
+                           return new Text(
+                             data.data.toString(),
+                             style:new TextStyle(
+                               color: Colors.blueAccent
+                             )
+                           );
+                         }else{
+                           return new Text("no data Saved");
+                         }
+                       },
+                     )
                    ],
                  ),
                )
