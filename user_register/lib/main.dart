07a661308ed8401a6aa2a8f927ffa65af92e0a48
hashika <m.g.hashikamaduranga@gmail.com>
2019-10-26 22:23:28 +0530

    create login app

---
+++ user_register/lib/main.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+import 'ui/home.dart';
+
+void main(){
+  
+  runApp(new MaterialApp(
+    title: "user register",
+    home: new Home(),
+  ));
+}
---
+++ user_register/lib/ui/home.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  Home({Key key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final TextEditingController _inputusercontroller =new TextEditingController();
+  final TextEditingController _inpputpasswordc =new TextEditingController();
+
+  _login(){
+    setState(() {
+     print("hashika"); 
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       appBar: new AppBar(
+         title: new Text("Login"),
+         backgroundColor: Colors.black26,
+         centerTitle: true,
+
+       ),
+       body: Stack(
+         alignment: Alignment.topCenter,
+           children: <Widget>[
+             new Center(
+              child:new  Image.asset("images/img.jpeg",width: 479,height: 1100,fit: BoxFit.fill,),
+             ),
+               new Column(
+                 
+                 children: <Widget>[
+                   
+                   Image.asset("images/face2.jpg",width:100,height: 100.0,)
+                 ],
+                 
+               ),
+             Container(
+               margin: const EdgeInsets.only(top: 150.0),
+               width: 380.0,
+               height: 180.0,
+               color: Colors.grey,
+               child: Column(
+                 children: <Widget>[
+
+                    new TextField(
+                    controller: _inputusercontroller ,
+                    decoration: InputDecoration(
+                      hintText: "Username",
+                      icon: Icon(Icons.people)
+                    ),
+                  ),
+                  new TextField(
+                    controller: _inputusercontroller,
+                    decoration: new InputDecoration(
+                      hintText: "Password",
+                      icon: Icon(Icons.person_pin)
+                    ),
+                  ),
+                 
+                  new Center(
+                    child: Column(
+                      children: <Widget>[
+                         new Padding(padding:const EdgeInsets.all(10.5) ,),
+                         new RaisedButton(
+                          onPressed:_login(),
+                          color: Colors.redAccent,
+                          child: new Text("Login",style: TextStyle(color: Colors.white),),
+
+
+                        )
+                      ],
+                    )
+                  )
+                 
+                 ],
+               )
+              
+               
+               
+              
+             )
+             
+           ],
+         ),
+       
+    );
+  }
+}
